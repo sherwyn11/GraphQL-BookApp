3b55dd2f9cd708df505898396b8a436e077206a1
Sherwyn Dsouza <sherwyndsouza1999@gmail.com>
2020-08-21 18:42:02 +0530

    :rocket: project deployed

--- server/db/mongoose.config.js
+++ server/db/mongoose.config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config();
 
 mongoose.connect(process.env.MONGOLAB_URI, { 
     useNewUrlParser: true, 
