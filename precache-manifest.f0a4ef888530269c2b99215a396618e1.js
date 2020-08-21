41b63c784e0cfa4a050700c398937e5ab27e5ba2
Sherwyn Dsouza <sherwyndsouza1999@gmail.com>
2020-08-21 18:33:08 +0530

    Updates

---
+++ precache-manifest.f0a4ef888530269c2b99215a396618e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "158bbb2e04548d987f8a06ff74699515",
+    "url": "/GraphQL-BookApp/index.html"
+  },
+  {
+    "revision": "c0f89899406f5dd56868",
+    "url": "/GraphQL-BookApp/static/css/main.33b467b0.chunk.css"
+  },
+  {
+    "revision": "647c15c1bba631cbd219",
+    "url": "/GraphQL-BookApp/static/js/2.0febf64f.chunk.js"
+  },
+  {
+    "revision": "23e75ecee431f5dfb96fa2deb80f243b",
+    "url": "/GraphQL-BookApp/static/js/2.0febf64f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0f89899406f5dd56868",
+    "url": "/GraphQL-BookApp/static/js/main.566bf6d8.chunk.js"
+  },
+  {
+    "revision": "c4bd788e12deafd63cc7",
+    "url": "/GraphQL-BookApp/static/js/runtime-main.4d0f0d6a.js"
+  }
+]);
